173269e9a2f994c416545fd6f3755791a5a67cad
daffa@MacStudio <daffa.akbar.aprilio.p8@dc.tohoku.ac.jp>
2026-05-07 16:46:31 +0900

    (Ongoing) Python script to convert SORBI, ENSRNA from SnpEFF annotated VCF

---
+++ workflow/scripts/snpeff_output_convert2EGI.py
@@ -0,0 +1,16 @@
+# #!/Users/daffa/miniconda3/envs/sbi/bin/python3
+
+# # convert the gene IDs of the SnpEFF-annotated VCF to standard gene ID
+
+# import argparse
+# import os
+
+# CONVERSION_KEY = 
+
+# def main():
+#     parser = argparse.ArgumentParser(description="Visualize coverage depth per chromosome.")
+#     parser.add_argument("--input", required=True, help="Path to the annotated VCF file, output of SnpEFF")
+#     parser.add_argument("--output", required=True, help="Path of the output plot file")
+#     args = parser.parse_args()
+
+# def 
